ffe3db25ec0d390e1765e6dd59730980513d2f5f
buanet <andre@buanet.de>
2021-03-14 15:59:05 +0100

    testing

--- stage-docker/01-install-docker/01-run.sh
+++ stage-docker/01-install-docker/01-run.sh
@@ -1,15 +1,8 @@
 #!/bin/bash -e
 
-echo "00-run.sh is running"
-
 # install docker
 on_chroot << EOF
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker docker
-EOF
-
-# install docker-compose
-on_chroot << EOF
-pip3 -v install docker-compose
 EOF
